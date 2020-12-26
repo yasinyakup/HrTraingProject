a505680fc8b6ac043ac723f6108eec5e25b0033c
yasinyakup <yasinyakup@gmail.com>
2020-12-26 12:54:08 +0300

    small updates

--- src/main/java/com/yaytech/loginjwt/security/jwt/JwtUtils.java
+++ src/main/java/com/yaytech/loginjwt/security/jwt/JwtUtils.java
@@ -14,10 +14,10 @@ import java.util.Date;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
+	@Value("${yaytech.login.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
+	@Value("${yaytech.login.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
